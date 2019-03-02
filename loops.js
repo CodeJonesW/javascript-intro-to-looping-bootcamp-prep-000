9807935537c66cdfb1747447457f359474730d87
Will Jones <willjones2010@gmail.com>
2019-03-02 22:05:06 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -24,10 +24,8 @@ function incrementVariable() {
 }
 
 function doWhileLoop (num) {
-  for (var i = 0; i < 25; i++) {
   do {console.log("I run once regardless.");
 } while (incrementVariable(i) < num);
-}
 }
 
 
