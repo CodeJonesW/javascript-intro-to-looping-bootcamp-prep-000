231446cd6356d65f3650582594221ad136cf4f01
Will Jones <willjones2010@gmail.com>
2019-03-02 21:37:13 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,14 +3,24 @@ function whileLoop(n) {
     console.log(--n);
  }
  return 'done';
-} 
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
 function incrementVariable() {
   i = i + 1;
   return i;
 }
+
 function doWhileLoop (num) {
   for (var i = 0; i < num; i++) { 
   do { console.log('I run once regardless.');
 } while (incrementVariable(i) < num);
 }
 }
+
+
+
+
